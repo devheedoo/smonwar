4ced89c92218ac564f34f5feec8d337b024cf6ab
devheedoo <devheedoo@gmail.com>
2019-09-29 02:22:49 +0900

    Updates

---
+++ precache-manifest.f2b10d8cbcab6db88c07bbcc404a37c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dee7ace200f18f0583ba894394f112",
+    "url": "/smonwar/index.html"
+  },
+  {
+    "revision": "aa893a437316fde092e8",
+    "url": "/smonwar/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "05fa71e86e925aa50c30",
+    "url": "/smonwar/static/js/2.4a3018c5.chunk.js"
+  },
+  {
+    "revision": "aa893a437316fde092e8",
+    "url": "/smonwar/static/js/main.4c980d81.chunk.js"
+  },
+  {
+    "revision": "863fd52b6f9a8ff9d53f",
+    "url": "/smonwar/static/js/runtime-main.d94f7a9f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/smonwar/static/media/logo.25bf045c.svg"
+  }
+]);
